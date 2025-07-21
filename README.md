2da970f1582e1a396ca49b931769f549c1733a6c
Your Name <jomerubaldo@gmail.com>
2025-07-22 03:07:02 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# student-honor-checker
+# 🎯 Project Title: "Student Honor Checker"
+
+## A small program that checks if a list of students has honors or not, based on their grades.
---
+++ function.js
@@ -0,0 +1,49 @@
+// You have a list of student objects (name and grade).
+const listStudent = [
+  {
+    name: 'Jomer',
+    grade: 90,
+  },
+  {
+    name: 'Abby',
+    grade: 86,
+  },
+  {
+    name: 'Lerma',
+    grade: 91,
+  },
+  {
+    name: 'Kimberly',
+    grade: 87,
+  },
+  {
+    name: 'Hanna',
+    grade: 76,
+  },
+];
+
+const studentWithHonors = [];
+
+// for of loop
+// You cannot directly use for...of on an object, because for...of works only on iterables like arrays.
+for (let list of listStudent) {
+  // destructuring each students detail in object
+
+  const { name, grade } = list;
+
+  // spread with ternary
+  const isStudent = grade >= 90 ? true : false;
+
+  // spread with ternary
+  const updateStudent = {
+    ...list,
+    honors: isStudent ? 'With Honors' : 'No Honors',
+  };
+
+  // push to studenWithHonoor empty array to add the value
+  studentWithHonors.push(updateStudent);
+}
+// for of loop for final
+for (const s of studentWithHonors) {
+  console.log(`${s.name} - ${s.honors}`);
+}
